425e7be5fd7eb13b309fa2f078f0672a6ec33c19
Alexandre Huat <alexandre.huat@gmail.com>
2017-12-06 11:08:17 +0100

    gfl_cw

--- gfl/base.py
+++ gfl/base.py
@@ -6,6 +6,9 @@ class GroupFusedLasso(metaclass=abc.ABCMeta):
     Base class for the group fused Lasso.
     """
 
+    def __init__(self, lambda_):
+        self.lambda_ = lambda_
+
     @abc.abstractmethod
     def detect(self, Y, **kwargs):
         """
